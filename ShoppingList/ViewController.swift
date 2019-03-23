5c1226f358660b239cfa32ad9df2834a6c924d6d
Usuário Convidado <Guest@PA06MICRO06.local>
2019-03-23 10:54:25 -0300

    Initial

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Eric Brito on 23/03/19.
+//  Copyright © 2019 FIAP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    @IBOutlet weak var tfName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
