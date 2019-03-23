9c21cf4d929af5854844d16737d84e932e0b5c17
Usuário Convidado <Guest@PA06MICRO06.local>
2019-03-23 14:48:37 -0300

    Initial

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -14,10 +15,60 @@ class ViewController: UIViewController {
     @IBOutlet weak var tfPassword: UITextField!
     @IBOutlet weak var tfName: UITextField!
     
+    var handle: AuthStateDidChangeListenerHandle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
+            print("Usuário logado:", user?.email ?? "")
+            if let user = user {
+                self.showMainScreen(user: user, animated: false)
+            }
+        })
+        
     }
 
-
+    func performUserChange(user: User?) {
+        guard let user = user else { return }
+        let changeResquest = user.createProfileChangeRequest()
+        changeResquest.displayName = tfName.text
+        changeResquest.commitChanges { (error) in
+            if error != nil {
+                print(error!)
+            }
+            self.showMainScreen(user: user)
+        }
+    }
+    
+    func showMainScreen(user: User?, animated: Bool = true) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: String(describing: ShoppingTableViewController.self)) else { return }
+        navigationController?.pushViewController(vc, animated: animated)
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        handle = nil
+        
+        Auth.auth().signIn(withEmail: tfEmail.text!, password: tfPassword.text!) { (result, error) in
+            if error == nil {
+                self.performUserChange(user: result?.user)
+            } else {
+                print(error!)
+            }
+        }
+    }
+    
+    @IBAction func signup(_ sender: Any) {
+        handle = nil
+        
+        Auth.auth().createUser(withEmail: tfEmail.text!, password: tfPassword.text!) { (result, error) in
+            if error == nil {
+                self.performUserChange(user: result?.user)
+            } else {
+                print(error!)
+            }
+        }
+    }
+    
 }
 
